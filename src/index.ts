8892d9fedaca17d2793b1e9d6900c6a150dad0fd
Fabricio Futryk <ffutryk52@gmail.com>
2022-05-27 01:39:02 -0300

    fix: removed backend

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import dotenv from "dotenv";
-import { HttpServer } from "./infrastructure/webserver/HttpServer";
-
-dotenv.config();
-
-const server: HttpServer = new HttpServer();
-
-server.listen();
