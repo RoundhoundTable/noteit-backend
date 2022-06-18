f9513db8c736e0802a86c5d4968d4d6169e2d6c8
Fabricio Futryk <ffutryk52@gmail.com>
2022-06-18 20:01:19 -0300

    feat: Add cloud storage for pictures

---
+++ src/application/CloudStorage.ts
@@ -0,0 +1,43 @@
+import {
+  FirebaseStorage,
+  getDownloadURL,
+  getStorage,
+  ref,
+  UploadResult,
+  uploadString,
+} from "firebase/storage";
+import { EFileStringFormat } from "../domain/enumerators/EFileStringFormat";
+import { IFile } from "../domain/interfaces/IFile";
+import { app } from "../infrastructure/cloud/firebase";
+
+class CloudStorage {
+  private readonly storage: FirebaseStorage;
+
+  constructor() {
+    this.storage = getStorage(app);
+  }
+
+  async upload(
+    file: IFile,
+    format: EFileStringFormat = EFileStringFormat.base64
+  ): Promise<UploadResult> {
+    try {
+      const storageRef = ref(this.storage, `profile-pictures/${file.name}.jpg`);
+      return await uploadString(storageRef, file.data, format);
+    } catch (e) {
+      throw new Error("ERROR UPLOADING IMAGE");
+    }
+  }
+
+  async getDownloadURL(filename: string): Promise<string> {
+    try {
+      const storageRef = ref(this.storage, `profile-pictures/${filename}.jpg`);
+      return await getDownloadURL(storageRef);
+    } catch (e) {
+      console.log(e);
+      throw new Error("ERROR GETTING IMAGE");
+    }
+  }
+}
+
+export default new CloudStorage();
---
+++ src/domain/enumerators/EFileStringFormat.ts
@@ -0,0 +1,5 @@
+export enum EFileStringFormat {
+  base64 = "base64",
+  base64url = "base64url",
+  dataUrl = "data_url",
+}
