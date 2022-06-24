d86e73ce63d6c482f513755083c939af585d7e3e
Fabricio Futryk <ffutryk52@gmail.com>
2022-06-23 23:11:24 -0300

    refactor: Remove existence check ; Edit update method

--- src/application/services/UserService.ts
+++ src/application/services/UserService.ts
@@ -1,4 +1,4 @@
-import { DeepPartial, Repository } from "typeorm";
+import { DeepPartial, FindOptionsWhere, Repository } from "typeorm";
 import { Entities } from "../../domain/entities";
 import { InjectRepository } from "../decorators/InjectRepository";
 
@@ -6,19 +6,22 @@ export class UserService {
   @InjectRepository(Entities.User)
   private readonly userRepository: Repository<Entities.User>;
 
-  async create(payload: DeepPartial<Entities.User>, accountId: string) {
-    if (!this.userRepository.findOne({ where: { username: payload.username } }))
-      throw new Error("USERNAME ALREADY IN USE");
-
+  async create(payload: DeepPartial<Entities.User>) {
     const user: Entities.User = this.userRepository.create({
       ...payload,
-      accountId,
     });
 
     return await this.userRepository.save(user);
   }
 
   async update(payload: DeepPartial<Entities.User>, username: string) {
-    return await this.userRepository.update(username, payload);
+    return await this.userRepository.update(
+      <FindOptionsWhere<Entities.User>>{
+        where: {
+          username,
+        },
+      },
+      payload
+    );
   }
 }
