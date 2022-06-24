8fe7c00813a055602512f0a1bd09aad83038948a
Fabricio Futryk <ffutryk52@gmail.com>
2022-06-23 23:12:51 -0300

    feat: Add GraphQL Express server

--- src/infrastructure/webserver/HttpServer.ts
+++ src/infrastructure/webserver/HttpServer.ts
@@ -1,24 +1,63 @@
-import express, { Express } from "express";
+import express from "express";
+import { graphqlHTTP } from "express-graphql";
+import { buildSchema } from "type-graphql";
+import cors, { CorsOptions } from "cors";
+import { VoteResolver } from "../../interfaces/resolvers/VoteResolver";
+import { AuthResolver } from "../../interfaces/resolvers/AuthResolver";
+import { AppDataSource } from "../orm/typeorm/data-source";
+import { NoteResolver } from "../../interfaces/resolvers/NoteResolver";
+import { MembershipResolver } from "../../interfaces/resolvers/MembershipResolver";
+import { NotebookResolver } from "../../interfaces/resolvers/NotebookResolver";
+import { CommentResolver } from "../../interfaces/resolvers/CommentResolver";
+import { UserResolver } from "../../interfaces/resolvers/UserResolver";
 
 export class HttpServer {
   private port: number | string;
-  private app: Express;
+  private app: express.Express;
 
   constructor() {
+    const corsOptions: CorsOptions = {
+      credentials: true,
+      origin: "*",
+    };
+
     this.port = process.env.HTTP_SERVER_PORT || 3000;
     this.app = express();
 
     this.app.use(express.json());
     this.app.use(express.urlencoded({ extended: false }));
-
-    this.app.get("*", (_, res) => {
-      res.send("NoteIt Incoming");
-    });
+    this.app.use(cors(corsOptions));
   }
 
   async bootstrap() {
-    this.app.listen(this.port, () => {
-      console.log(`NoteIt server listening on port ${this.port}...`);
-    });
+    try {
+      const graphql = graphqlHTTP({
+        schema: await buildSchema({
+          resolvers: [
+            AuthResolver,
+            VoteResolver,
+            NoteResolver,
+            MembershipResolver,
+            NotebookResolver,
+            CommentResolver,
+            UserResolver,
+          ],
+        }),
+        context: ({ req, res }) => ({
+          req,
+          res,
+        }),
+        graphiql: true,
+      });
+
+      await AppDataSource.initialize();
+      this.app.use("/graphql", graphql);
+
+      this.app.listen(this.port, () => {
+        console.log(`NoteIt server listening on port ${this.port}...`);
+      });
+    } catch (e) {
+      console.log(e);
+    }
   }
 }
