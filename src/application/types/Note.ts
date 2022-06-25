665923ae5d65077f394a75d7f47507245e08e0cf
Fabricio Futryk <ffutryk52@gmail.com>
2022-06-25 20:49:42 -0300

    refactor: Remove Note output

--- src/application/types/Note.ts
+++ src/application/types/Note.ts
@@ -1,5 +1,4 @@
 import { Field, InputType } from "type-graphql";
-import { Entities } from "../../domain/entities";
 
 @InputType()
 export class NoteInput {
@@ -14,19 +13,4 @@ export class NoteInput {
 
   @Field({ nullable: false })
   notebookName: string;
-}
-
-export class NoteOutput {
-  id: string;
-  username: string;
-  notebookName: string;
-  content: string;
-  title: string;
-  createdOn: Date;
-  likedByUser?: boolean;
-  score?: number;
-  user?: Entities.User;
-  notebook?: Entities.Notebook;
-  votes?: Entities.Vote[];
-  comments?: Entities.Comment[];
 }
