b6e299576fefc18e58d440e0379656c1996c12a2
Fabricio Futryk <ffutryk52@gmail.com>
2022-07-09 04:42:13 -0300

    refactor: Remove try catch

--- src/application/CloudStorage.ts
+++ src/application/CloudStorage.ts
@@ -21,22 +21,13 @@ class CloudStorage {
     file: IFile,
     format: EFileStringFormat = EFileStringFormat.base64
   ): Promise<UploadResult> {
-    try {
-      const storageRef = ref(this.storage, `profile-pictures/${file.name}.jpg`);
-      return await uploadString(storageRef, file.data, format);
-    } catch (e) {
-      throw new Error("ERROR UPLOADING IMAGE");
-    }
+    const storageRef = ref(this.storage, `profile-pictures/${file.name}.jpg`);
+    return await uploadString(storageRef, file.data, format);
   }
 
   async getDownloadURL(filename: string): Promise<string> {
-    try {
-      const storageRef = ref(this.storage, `profile-pictures/${filename}.jpg`);
-      return await getDownloadURL(storageRef);
-    } catch (e) {
-      console.log(e);
-      throw new Error("ERROR GETTING IMAGE");
-    }
+    const storageRef = ref(this.storage, `profile-pictures/${filename}.jpg`);
+    return await getDownloadURL(storageRef);
   }
 }
 
