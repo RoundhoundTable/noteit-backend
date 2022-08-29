72dc1bbdaa628056cf513e066c73ea67782d108d
Fabricio Futryk <ffutryk52@gmail.com>
2022-08-29 15:22:21 -0300

    feat: Add mutation handlers typing

---
+++ src/application/types/Handlers.ts
@@ -0,0 +1,41 @@
+import { PrismaClient, User } from "@prisma/client";
+import {
+  ENoteMutationType,
+  ENotebookMutationType,
+  EMembershipMutationType,
+  ECommentMutationType,
+  EUserMutationType,
+} from "../enumerators/EMutationTypes";
+
+export type MutationHandlerFunc<T, R> = (
+  payload: T,
+  prisma: PrismaClient,
+  user: User
+) => R;
+export type MutationHandler<
+  T extends
+    | ENoteMutationType
+    | ENotebookMutationType
+    | EMembershipMutationType
+    | ECommentMutationType
+    | EUserMutationType,
+  T1,
+  R
+> = Record<T, MutationHandlerFunc<T1, R>>;
+
+export type CreateResult = Promise<{ created: string }>;
+export type EditResult = Promise<{ edited: boolean }>;
+export type DeleteResult = Promise<{ deleted: boolean }>;
+export type KickResult = Promise<{ kicked: boolean }>;
+export type JoinResult = Promise<{ joined: boolean }>;
+export type LeaveResult = Promise<{ left: boolean }>;
+
+export type NoteHandlerResult = CreateResult | EditResult | DeleteResult;
+export type NotebookHandlerResult = CreateResult | EditResult | DeleteResult;
+export type MembershipHandlerResult =
+  | JoinResult
+  | LeaveResult
+  | KickResult
+  | EditResult;
+export type CommentHandlerResult = CreateResult | DeleteResult;
+export type UserHandlerResult = EditResult;
