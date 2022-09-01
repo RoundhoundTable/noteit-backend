84897e5841d3bf4f393511612023775145088b0a
Fabricio Futryk <ffutryk52@gmail.com>
2022-08-31 22:11:44 -0300

    fix: Allow comment content to have non-alphanum chars

--- src/application/validation/Comment.ts
+++ src/application/validation/Comment.ts
@@ -2,10 +2,9 @@ import Joi from "joi";
 import { joiMessages } from "./errorMessages";
 
 export const createSchema = Joi.object().keys({
-  content: Joi.string().alphanum().min(1).max(255).required().messages({
+  content: Joi.string().min(1).max(255).required().messages({
     "string.min": joiMessages.MIN_CHAR,
     "string.max": joiMessages.COMMENT_MAX_CHAR,
-    "string.alphanum": joiMessages.NO_ALPHANUM,
     "any.required": joiMessages.EMPTY_FIELD,
   }),
   noteId: Joi.string().uuid().required().messages({
